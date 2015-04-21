72e7e90b88d437143e340dccdecb0594d360dec8
Daniel Londoño <danielpunk@gmail.com>
2015-04-21 16:20:48 -0500

    termino header

--- index.php
+++ index.php
@@ -3,10 +3,78 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Turf Paint</title>
-	<link rel="stylesheet" href="bootstrap.css">
-	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	<link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
 </head>
 <body>
+	<header>
+		<div class="container">
+			<div class="row logo">
+				<div class="col-md-12">
+					<a href="">
+						<img src="img/logo.png" alt="" width="231">
+					</a>
+				</div>
+			</div>
+
+			<div class="row content">
+				<div class="col-md-10 col-md-offset-1 " id="main">
+					<div class="row text-center">
+						<div class="col-md-5 ">
+							<img src="img/endurant-organic-colorants.jpg" alt="">
+						</div>
+						<div class="col-md-7 colors">
+							<img src="img/colors.jpg" alt="">
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-10 col-md-offset-1 buynow">
+					<div class="line"></div>
+					<a href="">BUY NOW</a>
+				</div>
+
+
+
+				<div class="col-md-8 col-md-offset-2 intro">
+					<p>
+						<strong>Endurant organic colorants</strong> paint will save you time and money on lawn maintenance and lawn repair. Endurant turf paint is the professional's choice in lawn colorant due to its natural color. Lawn paint gives wow factor to your lawn, particularly in times of winter dormancy, drought, listing a property for sale or instead of overseeding. Nobody wants brown grass! So get rid of it in minutes with environmentally friendly yard paint that can last as long as three months, while improving soil and plant health.
+					</p>
+				</div>
+
+			</div>
+		</div>
+	</header>
+
+
+	<section>
+		
+			<nav>
+				<div class="container">
+					<ul>
+						<li class="active">
+							<a href="">Home</a>
+						</li>
+						<li>
+							<a href="">Applications</a>
+						</li>
+						<li>
+							<a href="">How to use</a>
+						</li>
+						<li>
+							<a href="">Gallery</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+
+
+		</div>
+	</section>
+
+
 	
 
 
