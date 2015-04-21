c02318dcbfead482000b4d366fdbfbae0432337d
Daniel Londoño <danielpunk@gmail.com>
2015-04-21 14:51:09 -0500

    Creo archivo base

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Turf Paint</title>
+	<link rel="stylesheet" href="bootstrap.css">
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	
+
+
+
+</body>
+</html>
